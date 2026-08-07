caf5833937cd270a27b6fe20f2c433ef01fd9c9e
imbytecat <imbytecat@gmail.com>
2026-08-08 01:20:34 +0800

    fix(nix): load GTK schemas and media plugins
    WebKitGTK read invalid display scaling in terminal shells without GTK schemas. Expose GStreamer plugins needed for microphone capture too.

--- flake.nix
+++ flake.nix
@@ -12,6 +12,16 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
+          gstreamerPlugins = with pkgs.gst_all_1; [
+            gstreamer
+            gst-plugins-base
+            gst-plugins-good
+            gst-plugins-bad
+          ];
+          gsettingsSchemaPath = pkgs.lib.concatStringsSep ":" [
+            "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+            "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+          ];
           runtimeLibraries = with pkgs; [
             atk
             cairo
@@ -30,24 +40,31 @@
         in
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              curl
-              file
-              gcc
-              glib.dev
-              gtk3.dev
-              libayatana-appindicator.dev
-              librsvg.dev
-              libsoup_3.dev
-              libxkbcommon.dev
-              mise
-              openssl.dev
-              pkg-config
-              webkitgtk_4_1.dev
-              wget
-            ];
+            packages =
+              (with pkgs; [
+                curl
+                file
+                gcc
+                glib.dev
+                gsettings-desktop-schemas
+                gtk3.dev
+                libayatana-appindicator.dev
+                librsvg.dev
+                libsoup_3.dev
+                libxkbcommon.dev
+                mise
+                openssl.dev
+                pkg-config
+                webkitgtk_4_1.dev
+                wget
+              ])
+              ++ gstreamerPlugins;
 
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibraries;
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (runtimeLibraries ++ gstreamerPlugins);
+            GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.makeSearchPath "lib/gstreamer-1.0" gstreamerPlugins;
+            shellHook = ''
+              export XDG_DATA_DIRS="${gsettingsSchemaPath}:''${XDG_DATA_DIRS:-}"
+            '';
           };
         }
       );
